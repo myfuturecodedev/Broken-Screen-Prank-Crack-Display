6ec4e3b4eb0ee31c4d95b773d8b8e67f0faa2700
brijkishorkumar <brijkishor.kumar@myfuturecode.com>
2026-06-23 15:34:27 +0530

    This commit after broken screen feature without sound.

---
+++ app/src/main/java/com/futurecode/crackdisplayprank/ui/afterlogin/PreviewScreenFragment.kt
@@ -0,0 +1,21 @@
+package com.futurecode.crackdisplayprank.ui.afterlogin
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import com.futurecode.crackdisplayprank.R
+import com.futurecode.crackdisplayprank.base.BaseFragment
+import com.futurecode.crackdisplayprank.databinding.FragmentPreviewScreenBinding
+
+
+class PreviewScreenFragment : BaseFragment<FragmentPreviewScreenBinding>(FragmentPreviewScreenBinding::inflate) {
+
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+
+    }
+}
